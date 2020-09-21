90bc3188f82a529d7354acc79dfa6dfb7e3d96a1
Jay <jaykamas@gmail.com>
2020-09-21 18:11:25 -0400

    Converts database to posgresql from sqlite3.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_09_21_165848) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "birds", force: :cascade do |t|
     t.string "name"
     t.integer "age"
