cd571ed650f3ba559618fbac65317e960cd14a4d
Jay <jaykamas@gmail.com>
2020-09-21 13:04:22 -0400

    Add Birds resource with contoller action and 3 seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Bird.create(name: "Fluffy", age: 3)
+Bird.create(name: "Red", age: 15)
+Bird.create(name: "Snarky", age: 27)
